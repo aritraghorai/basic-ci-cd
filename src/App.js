7edeae0cd75a07563b57c8dd43d43bb1a7d569c3
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2022-01-20 18:22:08 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Aritra Ghorai From Deployment
+        Aritra Ghorai From Vercel
       </header>
     </div>
   );
